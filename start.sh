38d166fbda05121429f7cf0302e6b2a412432b1a
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-06-08 02:19:47 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+# 启动xray
+nohup ./web run -config ./config.json >/dev/null 2>&1 &
+
 # 启动cf tunnel
 nohup ./argo tunnel --edge-ip-version auto run --token $TOKEN  >/dev/null 2>&1 &
-
-# 启动xray
-nohup ./web run -c ./config.json >/dev/null 2>&1 &
 
 # 启动Python应用程序
 python3 main.py
@@ -15,4 +15,3 @@ ps -ef
 echo "----- 系统信息...----- ."
 cat /proc/version
 echo "----- good luck (kid).----- ."
-sleep 1000000000000000000000000000
