1f83810deaa9c34e0a6d1fd1123d28f69c0016b0
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-06-07 22:56:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def hello_world():
     return 'Hello, World!'
 
 def start_tunnel_and_xray():
-    subprocess.call(['nohup', './argo', 'tunnel', '--edge-ip-version', 'auto', 'run', '--token', '$Token', '>/dev/null', '2>&1', '&'])
-    subprocess.call(['nohup', './xray', '-c', './config.json', '>/dev/null', '2>&1', '&'])
+    subprocess.call(['nohup', '/app/argo', 'tunnel', '--edge-ip-version', 'auto', 'run', '--token', '$Token', '>/dev/null', '2>&1', '&'])
+    subprocess.call(['nohup', '/app/xray', '-c', './config.json', '>/dev/null', '2>&1', '&'])
 
 if __name__ == '__main__':
     # 启动 Cloudflare Argo 隧道和 Xray 代理服务
