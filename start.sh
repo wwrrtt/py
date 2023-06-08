35ff0bf68b6f4d24aad37e10ba925aa9a79a7015
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-06-08 03:22:53 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
+UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
+sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g" config.json
+
 # 启动xray
 nohup ./web run -config ./config.json >/dev/null 2>&1 &
 
