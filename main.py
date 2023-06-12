43f1d28c5dbc0b018b8526f65da8021e7764ba26
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-06-12 10:28:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from flask import Flask
-import threading
-import subprocess
 
 app = Flask(__name__)
 
@@ -8,13 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-def start_tunnel_and_xray():
-    subprocess.call(['nohup', '/app/argo', 'tunnel', '--edge-ip-version', 'auto', 'run', '--token', '$Token', '>/dev/null', '2>&1', '&'])
-    subprocess.call(['nohup', '/app/xray', '-c', './config.json', '>/dev/null', '2>&1', '&'])
-
 if __name__ == '__main__':
-    # 启动 Cloudflare Argo 隧道和 Xray 代理服务
-    threading.Thread(target=start_tunnel_and_xray).start()
-
-    # 启动 Flask 应用程序
     app.run(host='0.0.0.0', port=5000)
